38170e812f52a5969c0f1cf228d3fc6eba6a0281
brettm12345 <brettmandler@gmail.com>
2020-03-09 03:05:37 -0400

    Delete leadermode

--- editor/index.ts
+++ editor/index.ts
@@ -1,8 +1,7 @@
 import * as core from './core';
 import * as history from './history';
 import * as keymap from './keymap';
-import * as leadermode from './leadermode';
 import * as terminal from './terminal';
 import * as vim from './vim';
 
-export { core, history, keymap, leadermode, terminal, vim };
+export { core, history, keymap, terminal, vim };
