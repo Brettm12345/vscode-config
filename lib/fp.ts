4fe8625d08e60083dd782ea6d3d84a8d53ad597c
brettm12345 <brettmandler@gmail.com>
2020-03-09 03:40:43 -0400

    Why is typescript dumb

--- lib/fp.ts
+++ lib/fp.ts
@@ -1,7 +1,11 @@
+import * as T from 'fp-ts/lib/Task';
 import { Task, task } from 'fp-ts/lib/Task';
 import { array } from 'fp-ts/lib/Array';
 import { flow, unsafeCoerce } from 'fp-ts/lib/function';
 
-export const flattenTasks: (
-  a: Task<void>[]
-) => Task<void> = flow(array.sequence(task), a => unsafeCoerce(a));
+const runAll: (a: void[]) => void = unsafeCoerce;
+
+export const flattenTasks: (a: Task<void>[]) => Task<void> = flow(
+  array.sequence(task),
+  T.map(runAll)
+);
