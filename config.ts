f167abc55035a304fc16c8d3f46ce2b4922fcf97
brettm12345 <brettmandler@gmail.com>
2020-03-09 03:40:23 -0400

    R E F A C T O R

--- config.ts
+++ config.ts
@@ -1,6 +1,5 @@
-import { configSet } from './lib';
+import { configSet, andThenSet } from './lib';
 import { pipe } from 'fp-ts/lib/pipeable';
-import * as T from 'fp-ts/lib/Task';
 
 export const init = pipe(
   configSet('editor', {
@@ -14,32 +13,26 @@ export const init = pipe(
     multiCursorModifier: 'ctrlCmd',
     tabSize: 2
   }),
-  T.chain(() =>
-    configSet('explorer', {
-      confirmDelete: false,
-      confirmDragAndDrop: false
-    })
-  ),
-  T.chain(() =>
-    configSet('files', {
-      autoSave: 'off',
-      exclude: {
-        '**/.DS_Store': true,
-        '**/.git': true,
-        '**/.hg': true,
-        '**/.history': true,
-        '**/.svn': true,
-        '**/CVS': true,
-        '**/node_modules': true
-      },
-      trimTrailingWhitespace: true
-    })
-  ),
-  T.chain(() =>
-    configSet('search', {
+  andThenSet('explorer', {
+    confirmDelete: false,
+    confirmDragAndDrop: false
+  }),
+  andThenSet('files', {
+    autoSave: 'off',
+    exclude: {
+      '**/.DS_Store': true,
+      '**/.git': true,
+      '**/.hg': true,
       '**/.history': true,
-      '**/bower_components': true,
+      '**/.svn': true,
+      '**/CVS': true,
       '**/node_modules': true
-    })
-  )
+    },
+    trimTrailingWhitespace: true
+  }),
+  andThenSet('search', {
+    '**/.history': true,
+    '**/bower_components': true,
+    '**/node_modules': true
+  })
 );
